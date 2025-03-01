f6206d26acd5d3fa1736d91b4b0a4f9e34731f88
권석찬 <harry@gwonseogchan-ui-noteubug.local>
2025-03-01 13:05:19 +0900

    다른 게임 샘플 추가

--- app/games/[id]/page.js
+++ app/games/[id]/page.js
@@ -11,7 +11,48 @@ const sampleGames = [
     description: "다양한 주제의 재미있는 퀴즈를 풀어보세요!",
     thumbnail: "/images/thumbnails/quiz.jpg",
   },
-  // 다른 게임들도 추가...
+  {
+    id: "memory",
+    title: "메모리 게임",
+    description: "카드를 짝지어 기억력을 테스트해보세요!",
+    thumbnail: "/images/thumbnails/memory.jpg",
+  },
+  {
+    id: "puzzle",
+    title: "퍼즐 게임",
+    description: "두뇌를 자극하는 퍼즐을 풀어보세요!",
+    thumbnail: "/images/thumbnails/puzzle.jpg",
+  },
+  {
+    id: "wordle",
+    title: "단어 맞추기",
+    description: "단어를 맞추는 워들 스타일 게임입니다.",
+    thumbnail: "/images/thumbnails/wordle.jpg",
+  },
+  {
+    id: "snake",
+    title: "스네이크 게임",
+    description: "뱀을 조종하여 먹이를 먹고 성장시키세요!",
+    thumbnail: "/images/thumbnails/snake.jpg",
+  },
+  {
+    id: "tetris",
+    title: "테트리스",
+    description: "클래식한 테트리스 게임을 즐겨보세요!",
+    thumbnail: "/images/thumbnails/tetris.jpg",
+  },
+  {
+    id: "tictactoe",
+    title: "틱택토",
+    description: "간단하지만 전략적인 틱택토 게임입니다.",
+    thumbnail: "/images/thumbnails/tictactoe.jpg",
+  },
+  {
+    id: "rockpaperscissors",
+    title: "가위바위보",
+    description: "컴퓨터와 가위바위보 대결을 펼쳐보세요!",
+    thumbnail: "/images/thumbnails/rockpaperscissors.jpg",
+  },
 ];
 
 export default function GamePage({ params }) {
